2c6d4eb8f23a8f720db6c49dab798b3efd36c48c
JhojanLt <jhojaninfant@gmail.com>
2025-03-24 21:06:58 -0500

    Github pages configuration

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/PortafolioWeb/',
   server: {
     //para usar remoto, cambiar localhost por la ip deseada
     host: 'localhost', // Asegura que escuche en la red
